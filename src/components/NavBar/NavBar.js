8853ed96f70034c482b3d5a6818c5c728eeafd03
Ani Khachatryan <anikhachatryan3@gmail.com>
2023-04-27 18:05:56 -0700

    Modify navbar to change to hamburger bar icon for certain screen sizes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import styles from './NavBar.module.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar,  } from 'react-bootstrap';
 import resume from '../../assets/AniKhachatryan_Resume.pdf';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 const NavBar = () => (
     <Navbar sticky='top' expand='lg' className={styles.navBar}>
       <Container fluid>
         <Navbar.Brand href='/' className={styles.navTitle}>Ani Khachatryan</Navbar.Brand>
-        <Nav className={styles.navItems}>
+        <NavbarToggle aria-controls='responsive-navbar-nav' />
+        <NavbarCollapse id='responsive-navbar-nav'>
+        <Nav className={`${styles.navItems} ms-auto`}>
           <Nav.Link href='/'>Home</Nav.Link>
           <Nav.Link href={resume} without rel='noreferrer' target='_blank'>Resume</Nav.Link>
         </Nav>
+        </NavbarCollapse>
       </Container>
     </Navbar>
 );
